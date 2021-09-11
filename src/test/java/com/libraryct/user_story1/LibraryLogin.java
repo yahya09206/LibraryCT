699e68ec2b06fbb08537c08e5d7c355cd10c2c74
Yahya <yahyahussein91@gmail.com>
2021-09-10 23:56:41 -0700

    wooooooow i had to pause so it could count

--- src/test/java/com/libraryct/user_story1/LibraryLogin.java
+++ src/test/java/com/libraryct/user_story1/LibraryLogin.java
@@ -14,7 +14,7 @@ public class LibraryLogin {
         // set up chrome driver
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("http://library2.cybertekschool.com/login.html");
+        driver.get("https://library2.cybertekschool.com/login.html");
 
         // verify librarian is on the login page
         if (driver.getTitle().equals("Login - Library")){
@@ -22,25 +22,33 @@ public class LibraryLogin {
         }else {
             System.out.println("Titles do not match, expected title is Login - Library");
         }
+        Thread.sleep(2000);
 
         // set up to sign in
         WebElement siginBox = driver.findElement(By.id("inputEmail"));
         siginBox.sendKeys("librarian47@library");
+        Thread.sleep(2000);
         WebElement passwordBox = driver.findElement(By.id("inputPassword"));
         passwordBox.sendKeys("Sdet2022*");
+        Thread.sleep(2000);
         WebElement submit = driver.findElement(By.xpath("//*[@id=\"login-form\"]/button"));
         submit.click();
 
-        List<WebElement> modules = driver.findElements(By.xpath("//li[@class='nav-item']"));
-        System.out.println("modules.size() = " + modules.size());
-        if (modules.size()==3){
-                System.out.println("Page displays 3 modules");
-            }else {
-                System.out.println("Failed, expected result must be 3, not " + modules.size());
-            }
+        Thread.sleep(2000);
+        List<WebElement> modules = driver.findElements(By.className("title"));
+        
+        if(modules.size()==3){
+            System.out.println("Test passed, actual number of modules are matching with expected result ");
+        }else {
+            System.out.println("Test failed. Actual number of modules are: "+modules.size());
+        }
 
+//        Thread.sleep(2000);
+//        for (WebElement eachModule : modules) {
+//            System.out.println("eachModule.getText() = " + eachModule.getText());
+//        }
         // quit driver
-        // driver.quit();
+        driver.quit();
     }
 }
 
