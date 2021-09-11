80739fc624e404d3e35c80fb2c80dbadacb9f8e6
Yahya <yahyahussein91@gmail.com>
2021-09-10 22:20:18 -0700

    navigating to website

--- src/test/java/com/libraryct/user_story1/LibraryLogin.java
+++ src/test/java/com/libraryct/user_story1/LibraryLogin.java
@@ -1,7 +1,15 @@
 package com.libraryct.user_story1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LibraryLogin {
     public static void main(String[] args) {
 
+        // set up chrome driver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://library2.cybertekschool.com/login.html");
     }
 }
