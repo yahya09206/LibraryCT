4d627bac35651473880f316e0193bb8e138a8823
Yahya <yahyahussein91@gmail.com>
2021-09-10 22:22:32 -0700

    setting up code to log in

--- src/test/java/com/libraryct/user_story1/LibraryLogin.java
+++ src/test/java/com/libraryct/user_story1/LibraryLogin.java
@@ -5,11 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LibraryLogin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // set up chrome driver
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://library2.cybertekschool.com/login.html");
+
+        // login
+
+        Thread.sleep(3000);
+        // quit driver
+        // driver.quit();
     }
 }
+
+//Credentials
+//librarian47@library pw: Sdet2022*
