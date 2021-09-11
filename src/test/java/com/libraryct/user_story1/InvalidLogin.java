f693a02e588d55f3bba194c27d0ca80bb08f50ef
Yahya <yahyahussein91@gmail.com>
2021-09-11 11:48:07 -0700

    printing out messsage from error

--- src/test/java/com/libraryct/user_story1/InvalidLogin.java
+++ src/test/java/com/libraryct/user_story1/InvalidLogin.java
@@ -1,7 +1,33 @@
 package com.libraryct.user_story1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class InvalidLogin {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://library2.cybertekschool.com/login.html");
+
+        //Enter invalid credentials
+        // set up to sign in
+        WebElement siginBox = driver.findElement(By.id("inputEmail"));
+        siginBox.sendKeys("librarian@");
+
+        WebElement passwordBox = driver.findElement(By.id("inputPassword"));
+        passwordBox.sendKeys("Sdet2022*");
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"login-form\"]/button"));
+        submit.click();
+
+        //Checking what error message says
+        WebElement errorMessage = driver.findElement(By.id("inputEmail-Error"));
+        System.out.println("errorMessage.getText() = " + errorMessage.getText());
+
+        //driver.quit();
     }
 }
