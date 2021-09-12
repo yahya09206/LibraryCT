e982ab627afe7edec22bf984897a97e760373294
Yahya <yahyahussein91@gmail.com>
2021-09-12 15:10:44 -0700

    wow i suck at this

--- src/test/java/com/libraryct/user_story1/InvalidLogin.java
+++ src/test/java/com/libraryct/user_story1/InvalidLogin.java
@@ -23,19 +23,21 @@ public class InvalidLogin {
 
         WebElement submit = driver.findElement(By.xpath("//*[@id=\"login-form\"]/button"));
         submit.click();
+        Thread.sleep(3000);
         
         //Checking what error message says
         WebElement errorMessage = driver.findElement(By.xpath("//*[@id=\"login-form\"]/div[2]/div"));
+        String alert = "Sorry, Wrong Email or Password";
+        String actualMessage = errorMessage.getText();
         System.out.println("errorMessage.getText() = " + errorMessage.getText());
+
         // Checking if correct error message is being displayed
-        if (errorMessage.equals("Sorry, Wrong Email or Password")){
-            System.out.println("Correct error message is printing");
+        if (alert.equals(actualMessage)){
+            System.out.println("match");
         }else {
-            System.out.println("Wrong error message is being displayed " + errorMessage.getText());
+            System.out.println("not match");
         }
 
-        Thread.sleep(3000);
-
-        //driver.quit();
+        driver.quit();
     }
 }
