20c8bea699939a4ea690e8463e7be3beb0f58962
Yahya <yahyahussein91@gmail.com>
2021-09-12 15:18:30 -0700

    entering student credentials

--- src/test/java/com/libraryct/user_story1/StudentLogin.java
+++ src/test/java/com/libraryct/user_story1/StudentLogin.java
@@ -1,11 +1,15 @@
 package com.libraryct.user_story1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class StudentLogin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -17,5 +21,28 @@ public class StudentLogin {
         } else {
             System.out.println("URL do not match, expected title is " + driver.getCurrentUrl());
         }
+
+        // set up to sign in
+        WebElement siginBox = driver.findElement(By.id("inputEmail"));
+        siginBox.sendKeys("student34@library");
+
+        WebElement passwordBox = driver.findElement(By.id("inputPassword"));
+        passwordBox.sendKeys("Sdet2022*");
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"login-form\"]/button"));
+        submit.click();
+
+        Thread.sleep(2000);
+        List<WebElement> modules = driver.findElements(By.className("title"));
+
+        if (modules.size() == 3) {
+            System.out.println("Test passed, actual number of modules are matching with expected result ");
+        } else {
+            System.out.println("Test failed. Actual number of modules are: " + modules.size());
+        }
+        Thread.sleep(2000);
+//        for (WebElement eachModule : modules) {
+//            System.out.println("eachModule.getText() = " + eachModule.getText());
+//        }
     }
 }
