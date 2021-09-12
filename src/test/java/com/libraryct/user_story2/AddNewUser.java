e51e0cac61b244d85ad72fc67d8677cb8c7e3dca
Yahya <yahyahussein91@gmail.com>
2021-09-12 16:04:49 -0700

    finally being added

--- src/test/java/com/libraryct/user_story2/AddNewUser.java
+++ src/test/java/com/libraryct/user_story2/AddNewUser.java
@@ -33,6 +33,7 @@ public class AddNewUser {
         // click add new user tab
         WebElement addNewUser = driver.findElement(By.xpath("//*[@id=\"users\"]/div[1]/div[1]/span/a"));
         addNewUser.click();
+        Thread.sleep(3000);
 
         // add info for new user
         WebElement fullName = driver.findElement(By.cssSelector("#add_user_form > div.modal-body > div > div:nth-child(1) > div > div > input"));
