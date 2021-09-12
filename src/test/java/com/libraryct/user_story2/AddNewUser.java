f02819d39ef418fdea7c2e10bd7e2d843a9e3841
Yahya <yahyahussein91@gmail.com>
2021-09-12 15:25:11 -0700

    navigating to website

--- src/test/java/com/libraryct/user_story2/AddNewUser.java
+++ src/test/java/com/libraryct/user_story2/AddNewUser.java
@@ -1,7 +1,14 @@
 package com.libraryct.user_story2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AddNewUser {
     public static void main(String[] args) {
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://library2.cybertekschool.com/login.html");
     }
 }
