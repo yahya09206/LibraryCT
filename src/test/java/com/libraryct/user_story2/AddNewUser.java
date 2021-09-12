3340bb8677a3baf3395f53b345b40a463c71cbbe
Yahya <yahyahussein91@gmail.com>
2021-09-12 15:26:45 -0700

    logging in as librarian

--- src/test/java/com/libraryct/user_story2/AddNewUser.java
+++ src/test/java/com/libraryct/user_story2/AddNewUser.java
@@ -1,7 +1,9 @@
 package com.libraryct.user_story2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AddNewUser {
@@ -10,5 +12,15 @@ public class AddNewUser {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://library2.cybertekschool.com/login.html");
+
+        // set up to sign in
+        WebElement siginBox = driver.findElement(By.id("inputEmail"));
+        siginBox.sendKeys("librarian47@library");
+
+        WebElement passwordBox = driver.findElement(By.id("inputPassword"));
+        passwordBox.sendKeys("Sdet2022*");
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"login-form\"]/button"));
+        submit.click();
     }
 }
