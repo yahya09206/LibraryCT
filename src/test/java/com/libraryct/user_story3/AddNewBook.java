5af4a0784f87d307e30d9568901ec80b8a4dcdc7
Yahya <yahyahussein91@gmail.com>
2021-09-16 11:18:48 -0700

    added code to set up driver and log in

--- src/test/java/com/libraryct/user_story3/AddNewBook.java
+++ src/test/java/com/libraryct/user_story3/AddNewBook.java
@@ -1,7 +1,28 @@
 package com.libraryct.user_story3;
 
-public class AddNewBook {
-    public static void main(String[] args) {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class AddNewBook {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://library2.cybertekschool.com/login.html");
+
+        // set up to sign in
+        WebElement siginBox = driver.findElement(By.id("inputEmail"));
+        siginBox.sendKeys("librarian47@library");
+
+        WebElement passwordBox = driver.findElement(By.id("inputPassword"));
+        passwordBox.sendKeys("Sdet2022*");
+
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"login-form\"]/button"));
+        submit.click();
+
+        Thread.sleep(3000);
     }
 }
