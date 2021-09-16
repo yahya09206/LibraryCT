0286aa580edfdca766f4a910fc55dfc6c21a0f76
Yahya <yahyahussein91@gmail.com>
2021-09-15 18:30:20 -0700

    added junit dependency

---
+++ src/test/java/com/libraryct/utility/TestBase.java
@@ -0,0 +1,33 @@
+package com.cybertek.utility;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class is meant to be super class
+ * to provide driver set up and closing browser
+ * for it's subclasses
+ */
+
+public abstract class TestBase {
+
+    protected WebDriver driver;
+
+    // setting up all driver stuff here directly in @BeforeEach method
+    @BeforeEach
+    public void setupWebDriver() {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+        driver = com.cybertek.utility.WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
