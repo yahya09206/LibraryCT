119b83cc9f219fa317dbbead7e1faccfcdb13d3b
Yahya <yahyahussein91@gmail.com>
2021-09-17 12:15:27 -0700

    was printing size of wrong variable

--- src/test/java/com/libraryct/user_story5/SelectUserGroups.java
+++ src/test/java/com/libraryct/user_story5/SelectUserGroups.java
@@ -26,14 +26,19 @@ public class SelectUserGroups extends TestBase {
         // click on user tab
         WebElement userTab = driver.findElement(By.xpath("//*[@id=\"menu_item\"]/li[2]/a/span[1]"));
         userTab.click();
-
+        Thread.sleep(3000);
         // select user group drown
         WebElement userGroupDropDown = driver.findElement(By.xpath("//*[@id=\"user_groups\"]"));
         userGroupDropDown.click();
-        List<WebElement> userGroupList = driver.findElements(By.name("//*[@id=\"user_groups\"]"));
-        userGroupDropDown.getSize();
+        Thread.sleep(3000);
+        List<WebElement> userGroupList = driver.findElements(By.xpath("//*[@id=\"user_groups\"]"));
+        System.out.println("userGroupList.size() = " + userGroupList.size());
 
-
+        if (userGroupList.size() == 3){
+            System.out.println("User group dropdown does contain 3 items");
+        }else {
+            System.out.println("Does not contain 3 items "  + userGroupList.size());
+        }
 
         Thread.sleep(3000);
 
