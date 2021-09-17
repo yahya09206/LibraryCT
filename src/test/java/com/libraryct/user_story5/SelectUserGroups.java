c42593c91026d29797772da810686d00b2df83b3
Yahya <yahyahussein91@gmail.com>
2021-09-17 12:00:06 -0700

    setting up class to select user group

--- src/test/java/com/libraryct/user_story5/SelectUserGroups.java
+++ src/test/java/com/libraryct/user_story5/SelectUserGroups.java
@@ -1,7 +1,14 @@
 package com.libraryct.user_story5;
 
 import com.libraryct.utility.TestBase;
+import org.junit.jupiter.api.Test;
 
 public class SelectUserGroups extends TestBase {
+
+    @Test
+    public void selectUserGroup(){
+
+
+    }
 
 }
