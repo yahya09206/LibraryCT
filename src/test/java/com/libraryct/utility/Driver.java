d0cb2df1dd99875fe3b82e35fefd231d75b2d1c2
Yahya <yahyahussein91@gmail.com>
2022-01-22 09:53:58 -0800

    static webdrive obj var

--- src/test/java/com/libraryct/utility/Driver.java
+++ src/test/java/com/libraryct/utility/Driver.java
@@ -1,10 +1,14 @@
 package com.libraryct.utility;
 
 
+import org.openqa.selenium.WebDriver;
+
 /**
  * Want to have a class with that only returns Single object
  * no matter how many times you asked for object
  * creating this class with technique from Singleton Pattern
  */
 public class Driver {
+
+    private static WebDriver obj;
 }
