6beba0b6a7cedea581245ad9bbea30a7591be4aa
Yahya <yahyahussein91@gmail.com>
2022-01-22 09:53:35 -0800

    notes to explain purpose

--- src/test/java/com/libraryct/utility/Driver.java
+++ src/test/java/com/libraryct/utility/Driver.java
@@ -1,4 +1,10 @@
 package com.libraryct.utility;
 
+
+/**
+ * Want to have a class with that only returns Single object
+ * no matter how many times you asked for object
+ * creating this class with technique from Singleton Pattern
+ */
 public class Driver {
 }
