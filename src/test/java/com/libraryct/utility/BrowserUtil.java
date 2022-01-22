cfc8a8cc55d513de5d0a8b1dbfde739d87a3d170
Yahya <yahyahussein91@gmail.com>
2022-01-22 09:48:10 -0800

    surround custom thread with try catch

--- src/test/java/com/libraryct/utility/BrowserUtil.java
+++ src/test/java/com/libraryct/utility/BrowserUtil.java
@@ -2,6 +2,14 @@ package com.libraryct.utility;
 
 public class BrowserUtil {
 
+    public static void waitFor(int seconds){
 
+        // sleep in seconds
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
