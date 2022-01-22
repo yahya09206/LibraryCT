e7fb04c1d1429c58050896b9eefc43051c250d05
Yahya <yahyahussein91@gmail.com>
2022-01-22 10:04:22 -0800

    setting up properties object for use in method

--- src/test/java/com/libraryct/utility/ConfigReader.java
+++ src/test/java/com/libraryct/utility/ConfigReader.java
@@ -1,9 +1,16 @@
 package com.libraryct.utility;
 
+import java.util.Properties;
+
 /**
  * A Utility class that load the keyvalue pair inside config.properties file
  * into Properties object and provide single method called
  * read to get the value out of the properties file we created.
  */
 public class ConfigReader {
+
+    // Declare properties object at class level, so it can be accessible in static method
+    private static Properties properties = new Properties();
+
+
 }
