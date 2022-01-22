742439fb7cbd2c1d94db1b91052b461b9b6e0534
Yahya <yahyahussein91@gmail.com>
2022-01-22 10:03:17 -0800

    utility class for read from files

---
+++ src/test/java/com/libraryct/utility/ConfigReader.java
@@ -0,0 +1,9 @@
+package com.libraryct.utility;
+
+/**
+ * A Utility class that load the keyvalue pair inside config.properties file
+ * into Properties object and provide single method called
+ * read to get the value out of the properties file we created.
+ */
+public class ConfigReader {
+}
