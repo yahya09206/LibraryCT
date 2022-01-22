b652ebe49411ee49ffdfe6ac6eabed87c05b0817
Yahya <yahyahussein91@gmail.com>
2022-01-22 09:51:04 -0800

    pass a param of locator and time to wait

--- src/test/java/com/libraryct/utility/BrowserUtil.java
+++ src/test/java/com/libraryct/utility/BrowserUtil.java
@@ -1,5 +1,7 @@
 package com.libraryct.utility;
 
+import org.openqa.selenium.By;
+
 public class BrowserUtil {
 
     public static void waitFor(int seconds){
@@ -12,6 +14,14 @@ public class BrowserUtil {
         }
     }
 
+    /**
+     * This method will check for visibility of element within the time given
+     * @param locator By.id or By.xpath or By.whatever
+     * @param timeToWait time to wait
+     * @return true if the element is found within the time and visible , false if not
+     */
+    public static boolean checkVisibilityOfElement(By locator, int timeToWait){
 
 
+    }
 }
