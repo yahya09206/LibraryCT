e02387d2cdfa92e138095b65b2deaae7afcfc602
Yahya <yahyahussein91@gmail.com>
2022-01-23 19:55:59 -0800

    test for logging in as student is passing

--- src/test/java/com/libraryct/tests/LoginTests.java
+++ src/test/java/com/libraryct/tests/LoginTests.java
@@ -1,15 +1,20 @@
 package com.libraryct.tests;
 
 import com.libraryct.pages.LoginPage;
+import com.libraryct.utility.BrowserUtil;
+import com.libraryct.utility.ConfigReader;
 import com.libraryct.utility.TestBase;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
 import org.junit.jupiter.api.Test;
 
 public class LoginTests extends TestBase {
 
     @Test
-    public void loginPage(){
+    public void loginToPageTest(){
 
         LoginPage loginPage = new LoginPage();
         loginPage.goTo();
+        loginPage.login(ConfigReader.read("username"), ConfigReader.read("password"));
+        BrowserUtil.waitFor(4);
     }
 }
