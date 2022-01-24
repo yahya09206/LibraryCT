ff1d58d7f2de71fee816e9b3c70e249689a1b41d
Yahya <yahyahussein91@gmail.com>
2022-01-23 19:42:27 -0800

    declaring fields for username password and submit button

--- src/test/java/com/libraryct/pages/LoginPage.java
+++ src/test/java/com/libraryct/pages/LoginPage.java
@@ -1,6 +1,16 @@
 package com.libraryct.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
 
+    @FindBy(xpath = "")
+    private WebElement usernameBox;
 
+    @FindBy(xpath = "")
+    private WebElement passwordBox;
+
+    @FindBy(xpath = "")
+    private WebElement submitBtn;
 }
