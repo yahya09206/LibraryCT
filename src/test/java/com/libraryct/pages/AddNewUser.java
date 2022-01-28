cd539f82c03edaf1bbd3f31b2d243b3e8a8d24fc
Yahya <yahyahussein91@gmail.com>
2022-01-27 21:20:33 -0800

    locating users tab by xpath

--- src/test/java/com/libraryct/pages/AddNewUser.java
+++ src/test/java/com/libraryct/pages/AddNewUser.java
@@ -1,4 +1,10 @@
 package com.libraryct.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AddNewUser {
+
+    @FindBy(xpath = "//li/a/span[text()='Users']")
+    private WebElement usersTab;
 }
