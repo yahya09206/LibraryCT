f02e132c288ee04279c0e922ccb52a0eab04f8d0
Yahya <yahyahussein91@gmail.com>
2022-01-27 21:21:29 -0800

    set up constructor with pagefactory

--- src/test/java/com/libraryct/pages/AddNewUser.java
+++ src/test/java/com/libraryct/pages/AddNewUser.java
@@ -1,10 +1,16 @@
 package com.libraryct.pages;
 
+import com.libraryct.utility.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AddNewUser {
 
     @FindBy(xpath = "//li/a/span[text()='Users']")
     private WebElement usersTab;
+
+    public AddNewUser(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
